8d55d6a2c8c5f21cc8de1ed9f0dd734683891dab
sam-mugo <keithgety@gmail.com>
2022-04-05 13:33:45 +0300

    add whitenoise file compression

--- codacy/settings.py
+++ codacy/settings.py
@@ -9,8 +9,6 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
