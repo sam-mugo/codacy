985c75aa046a341ddff775987ff162afe4a985e5
sam-mugo <keithgety@gmail.com>
2022-04-08 00:00:13 +0300

    add bootstrap 4

--- codacy/settings.py
+++ codacy/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = ''
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['codacy.herokuapp.com', '127.0.0.1']
 
